c546933c9bc82ce6bad0314fe8150ddd1dc3d4cb
jiyoung-Heo <jiyoungHeo.dev@gmail.com>
2024-06-14 17:35:52 +0900

    first commit

---
+++ src/test/java/com/example/learn_server/LearnServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
